28bc8efbc3823f43f3b09e4f28972515bac08285
Unknown <krys.nagorny@gmail.com>
2018-01-29 22:03:11 +0100

    + last build

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,21 +1,41 @@
 import {Component} from '@angular/core';
+import {trigger, state, style, transition, animate, keyframes, query, stagger} from '@angular/animations';
 
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('myAwesomeAnimation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ]),
+    ]),
+    // Define animations here.
+
+  ]
 })
 export class HeaderComponent {
+  state: string = 'small';
   //boolean for burger trigger
   isActive: boolean;
   constructor() {
     this.isActive = false;
+
   }
 
   burgerTrigger(){
     this.isActive =! this.isActive;
   }
 
-
+  test() {
+    console.log("test, test, test");
+  }
 }
--- src/components/plants/plant.component.ts
+++ src/components/plants/plant.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {Observable} from "rxjs/Observable";
 
 
@@ -7,20 +7,40 @@ import {Observable} from "rxjs/Observable";
   templateUrl: './plant.component.html',
   styleUrls: ['./plant.component.scss']
 })
-export class PlantComponent {
+export class PlantComponent implements AfterViewInit{
+  ngAfterViewInit(): void {
+    console.log("DOM fully loaded and parsed 2222");
+
+  }
   counter:number;
   showEyes:boolean;
+  isProcessing: boolean;
+  images;
   constructor(){
     this.counter = 1;
     this.showEyes = false;
+    this.isProcessing = true;
+    this.images = [];
     setInterval(()=>{
       console.log('show eyes');
       this.showEyes =!this.showEyes;
     },2000);
+    setInterval(()=>{
+      this.increase();
+    },5000);
 
   }
+    loadedImage(nr){
+    console.log('img loaded ', nr);
+    this.images.push(nr);
+    console.log('loading ', nr);
 
+    if(this.images.length === 1 || this.images.length >1){
+      this.isProcessing = false;
+    }
+  }
   increase(){
+    this.isProcessing = true;
     if(this.counter === 4){
       this.counter = 1;
     }else{
@@ -28,6 +48,8 @@ export class PlantComponent {
     }
   }
   decrease(){
+    this.isProcessing = true;
+
     if(this.counter === 1){
       this.counter = 4;
     }else{
